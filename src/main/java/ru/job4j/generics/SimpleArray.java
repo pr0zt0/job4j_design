855443f4f9b14c75a828620f1c32d37d94b1654c
Mikle <gumenyukmichael@gmail.com>
2021-06-30 19:48:13 +0300

    5.2.1. Реализовать SimpleArray<T> [#156 #172526] вывод только значемых элементов

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -40,7 +40,7 @@ public class SimpleArray<T> implements Iterable<T> {
 
             @Override
             public boolean hasNext() {
-                return point < data.length;
+                return point < size;
             }
 
             @Override
