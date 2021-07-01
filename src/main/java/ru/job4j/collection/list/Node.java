739202e1b923452b63c16cf3dce2a239bcc4cfad
Mikle <gumenyukmichael@gmail.com>
2021-07-01 22:48:09 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/list/Node.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection.list;
+
+public class Node<E> {
+    private E data;
+    public int index;
+    public Node<E> nodeNext;
+
+    public Node(E data) {
+        this.data = data;
+    }
+
+    public E getData() {
+        return data;
+    }
+}
---
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,69 @@
+package ru.job4j.collection.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private Node<E> nodeLast = null;
+    private Node<E> nodeFirst = null;
+    private int size = 0;
+    private int modCount = 0;
+
+    private boolean isEmpty() {
+        return nodeFirst == null;
+    }
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value);
+        if (isEmpty()) {
+            nodeFirst = newNode;
+        } else {
+            nodeLast.nodeNext = newNode;
+        }
+        nodeLast = newNode;
+        newNode.index = size;
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> node = nodeFirst;
+        while (node.index < size) {
+            if (node.index == index) {
+                break;
+            }
+            node = node.nodeNext;
+        }
+        return node.getData();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> curr = nodeFirst;
+            private int exModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return curr != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                } else if (exModCount != modCount) {
+                    throw new ConcurrentModificationException("mod array!");
+                }
+                E data = curr.getData();
+                curr = curr.nodeNext;
+                return data;
+            }
+        };
+    }
+}
