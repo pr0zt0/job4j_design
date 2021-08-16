e1df2630a136f866ee3f2824163d60485bda5e4b
Mikle <gumenyukmichael@gmail.com>
2021-08-16 20:43:08 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -14,7 +14,11 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("/?msg=Bye")) {
+                        if (str.contains("/?msg=Hello")) {
+                            out.write("Hello, dear friend.".getBytes());
+                        } else if (str.contains("/?msg=What")) {
+                            out.write("What ?".getBytes());
+                        } else if (str.contains("/?msg=Bye")) {
                             System.out.println("Down server!");
                             server.close();
                         }
