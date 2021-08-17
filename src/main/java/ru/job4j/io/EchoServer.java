f010d58858c2d18f1bbdb263cb1ec52372b55750
Mikle <gumenyukmichael@gmail.com>
2021-08-17 20:06:26 +0300

    1. Бот [#7921 #190696]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -15,14 +15,14 @@ public class EchoServer {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         if (str.contains("/?msg=Hello")) {
-                            out.write("Hello, dear friend.".getBytes());
-                        } else if (str.contains("/?msg=What")) {
-                            out.write("What ?".getBytes());
+                            out.write("Hello, dear friend".getBytes());
                         } else if (str.contains("/?msg=Bye")) {
                             System.out.println("Down server!");
                             server.close();
+                        } else if (str.contains("/?msg")
+                                && !str.contains("/?msg=Bye") && !str.contains("/?msg=Hello")) {
+                            out.write("What ?".getBytes());
                         }
-                        System.out.println(str);
                     }
                     out.flush();
                 }
