2c6a28fa867ae03f332dd99e18a4d1e8e181df72
Mikle <gumenyukmichael@gmail.com>
2021-08-17 23:18:57 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/json/Task.java
+++ src/main/java/ru/job4j/serialization/json/Task.java
@@ -1,8 +1,16 @@
 package ru.job4j.serialization.json;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "task")
 public class Task {
 
+    @XmlAttribute
     private boolean isClose;
+
+    public Task() {
+    }
 
     public Task(boolean isClose) {
         this.isClose = isClose;
--- src/main/java/ru/job4j/serialization/xml/Feature.java
+++ src/main/java/ru/job4j/serialization/xml/Feature.java
@@ -2,14 +2,41 @@ package ru.job4j.serialization.xml;
 
 import ru.job4j.serialization.json.Task;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringWriter;
+import java.text.DateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
+@XmlRootElement(name = "feature")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Feature {
-    private final int numStory;
-    private final String nameFeature;
-    private final Task task;
-    private final Date[] checkPoints;
+
+    @XmlAttribute
+    private int numStory;
+
+    @XmlAttribute
+    private String nameFeature;
+
+    private Task task;
+
+
+    @XmlElementWrapper(name = "checkPoints")
+    @XmlElement(name = "checkPoint")
+    private Date[] checkPoints;
+
+    @XmlAttribute
     private boolean isClose;
+
+    public Feature() {
+    }
 
     public Feature(int numStory, String nameFeature, Task task, Date[] checkPoints, boolean isClose) {
         this.numStory = numStory;
@@ -19,8 +46,21 @@ public class Feature {
         this.isClose = isClose;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
         Feature ft = new Feature(2, "New Mega Feature Ever", new Task(false),
-                new Date[]{new Date(2021, 01, 01), new Date(2031, 01, 01)}, false);
+                new Date[]{Date.from(Instant.now()), Date.from(LocalDate.parse("2032-12-31").
+                        atStartOfDay(ZoneId.of("Asia/Tokyo")).toInstant())}, false);
+
+        JAXBContext context = JAXBContext.newInstance(Feature.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(ft, writer);
+            String result = writer.getBuffer().toString();
+            System.out.println(result);
+        } catch (Exception e) {
+
+        }
     }
 }
