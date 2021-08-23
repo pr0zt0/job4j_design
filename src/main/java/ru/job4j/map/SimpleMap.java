384ffe7d96d6dae688d74e5f833f22437a4eedd3
Mikle <gumenyukmichael@gmail.com>
2021-08-23 21:53:07 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,161 @@
+package ru.job4j.map;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleMap<K, V> implements Map<K, V> {
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int capacity = 8;
+
+    private int count = 0;
+
+    private int modCount = 0;
+
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    @Override
+    public boolean put(K key, V value) {
+        if (count / capacity >= LOAD_FACTOR) {
+            expand();
+        }
+
+        int h = key.hashCode();
+        int hash = hash(h);
+        int index = indexFor(hash);
+
+        if (table[index] == null) {
+            enterEl(key, value, index);
+            return true;
+        } else {
+            if (table[index].key.equals(key)) {
+                table[index].value = value;
+                return true;
+            }
+            for (int i = index; i < table.length; i++) {
+                if (table[i] == null) {
+                    enterEl(key, value, index);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void enterEl(K key, V value, int index) {
+        MapEntry<K, V> el = new MapEntry<>(key, value);
+        table[index] = el;
+        count++;
+        modCount++;
+    }
+
+    private int hash(int hashCode) {
+        return hashCode % table.length;
+    }
+
+    private int indexFor(int hash) {
+        return hash & (table.length - 1);
+    }
+
+    private void expand() {
+        MapEntry<K, V>[] oldTable = table;
+        table = new MapEntry[oldTable.length * 2];
+        count = 0;
+        modCount = 0;
+        for (MapEntry<K, V> el: oldTable) {
+            if (el != null) {
+                put(el.key, el.value);
+            }
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        int h = key.hashCode();
+        int hash = hash(h);
+        int index = indexFor(hash);
+
+        MapEntry<K, V> el = table[index];
+
+        while (el != null && index < table.length) {
+            if (el.key.equals(key)) {
+                return el.value;
+            }
+            index++;
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        return false;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<K>() {
+            private MapEntry<K, V>[] curr = table;
+            private int cursor = 0;
+            private int exModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < curr.length && nextNotNull();
+            }
+
+            private boolean nextNotNull() {
+                boolean nextNull = false;
+                for (int i = cursor; i < curr.length; i++) {
+                    if (curr[i] != null) {
+                        nextNull = true;
+                        cursor = i;
+                        break;
+                    }
+                }
+                return nextNull;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                } else if (exModCount != modCount) {
+                    throw new ConcurrentModificationException("mod array!");
+                }
+                return curr[cursor++].key;
+            }
+        };
+    }
+
+    private static class MapEntry<K, V> {
+
+        K key;
+        V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            MapEntry<?, ?> mapEntry = (MapEntry<?, ?>) o;
+            return Objects.equals(key, mapEntry.key) && Objects.equals(value, mapEntry.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, value);
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.map;
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+import ru.job4j.collection.list.List;
+import ru.job4j.collection.list.SimpleLinkedList;
+
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SimpleMapTest {
+    @Test
+    public void whenAdd() {
+        SimpleMap<Integer, Integer> simpleMap = new SimpleMap();
+        simpleMap.put(1, 2);
+        assertThat(simpleMap.get(1), is(Integer.valueOf(2)));
+    }
+
+    @Test
+    public void whenAddSimilar() {
+        SimpleMap<Integer, Integer> simpleMap = new SimpleMap();
+        simpleMap.put(1, 2);
+        simpleMap.put(1, 3);
+        assertThat(simpleMap.get(1), is(Integer.valueOf(3)));
+    }
+
+    @Test
+    public void whenAddTwice() {
+        SimpleMap<Integer, Integer> simpleMap = new SimpleMap();
+        simpleMap.put(1, 2);
+        simpleMap.put(2, 3);
+        assertThat(simpleMap.get(1), is(Integer.valueOf(2)));
+        assertThat(simpleMap.get(2), is(Integer.valueOf(3)));
+    }
+
+    @Test
+    public void checkIterator() {
+        SimpleMap<Integer, Integer> simpleMap = new SimpleMap();
+        simpleMap.put(1, 5);
+        simpleMap.put(2, 3);
+
+        Iterator<Integer> first = simpleMap.iterator();
+        assertThat(first.hasNext(), Is.is(true));
+        assertThat(first.next(), Is.is(1));
+        assertThat(first.hasNext(), Is.is(true));
+        assertThat(first.next(), Is.is(2));
+        assertThat(first.hasNext(), Is.is(false));
+
+        Iterator<Integer> second = simpleMap.iterator();
+        assertThat(second.hasNext(), Is.is(true));
+        assertThat(second.next(), Is.is(1));
+        assertThat(second.hasNext(), Is.is(true));
+        assertThat(second.next(), Is.is(2));
+        assertThat(second.hasNext(), Is.is(false));
+    }
+}
