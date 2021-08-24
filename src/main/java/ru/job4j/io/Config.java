03e4d7a143996ef3477d431b5348e3e4d7af46ff
Mikle <gumenyukmichael@gmail.com>
2021-08-24 21:42:25 +0300

    формирование

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -2,7 +2,6 @@ package ru.job4j.io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOError;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
