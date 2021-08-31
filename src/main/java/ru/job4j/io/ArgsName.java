08ff1d293be0689520a04b2743ad510becdc12fd
Mikle <gumenyukmichael@gmail.com>
2021-08-31 21:10:10 +0300

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,42 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("No such key " + key);
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String s: args) {
+            String[] arrs = s.split("=");
+            if (arrs.length != 2) {
+                throw new IllegalArgumentException("Wrong input key jvm");
+            }
+            String key = arrs[0].replace('-', ' ').trim();
+            String val = arrs[1];
+            values.put(key, val);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
