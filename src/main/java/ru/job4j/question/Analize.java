517f82541c6ab48f64474ecba8385552fa35522e
Mikle <gumenyukmichael@gmail.com>
2021-10-20 02:13:29 +0300

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,32 @@
+package ru.job4j.question;
+
+import java.util.*;
+
+public class Analize {
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int added;
+        int changed = 0;
+        int deleted;
+        
+        Set<User> temp = new HashSet<>(previous);
+        Map<Integer, String> map = new HashMap<>();
+
+        for (User us : current) {
+            map.put(us.getId(), us.getName());
+        }
+
+        for (User user : previous) {
+            if (map.containsKey(user.getId()) && map.get(user.getId()).equals(user.getName())) {
+                map.remove(user.getId());
+                temp.remove(user);
+            } else if (map.containsKey(user.getId()) && !map.get(user.getId()).equals(user.getName())) {
+                changed++;
+            }
+        }
+
+        added = map.size() - changed;
+        deleted = temp.size() - changed;
+
+        return new Info(added, changed, deleted);
+    }
+}
